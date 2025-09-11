e7c0ecb287f013617ed57007d988d32758cd4806
Denis Karpov <karpovdenis.al@gmail.com>
2025-09-11 11:23:31 +0300

    Add lab 1

---
+++ lab1/main.py
@@ -0,0 +1,9 @@
+"""Программа Hello World для лабораторной работы."""
+
+def print_message() -> None:
+    """Процедура печати приветственного сообщения."""
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    print_message()
